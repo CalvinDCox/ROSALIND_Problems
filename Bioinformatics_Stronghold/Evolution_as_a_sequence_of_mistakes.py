e6b57ca0a3a7b0c1e52c90b6dc3834e16cf2f925
Calvin Dominique Cox III <calvinc@g.clemson.edu>
2017-10-30 21:56:17 -0400

    Update Evolution_as_a_sequence_of_mistakes.py
    Added comments

--- Bioinformatics_Stronghold/Evolution_as_a_sequence_of_mistakes.py
+++ Bioinformatics_Stronghold/Evolution_as_a_sequence_of_mistakes.py
@@ -6,16 +6,21 @@
 #Return: The Hamming distance dH(s,t)dH(s,t).
 
 #A little upset this one is so simple compared to the Counting GC content one
+#Readfile
 file = open('exampletext.txt')
 strains = file.readlines()
 
+#since we know what the input looks like we will insert the sequences into two separate variables right out
 dna1 = strains[0].rstrip("\n")
 dna2 = strains[1].rstrip("\n")
 
+#Declaring the important part that holds the Hamming Distance
 count = 0
 
+#Loops through both sequences, this only works if they are the same length though.
 for i,j in zip(dna1,dna2):
     if i != j:
         count += 1
-        
+
+#Print Hamming Distance        
 print(count)
