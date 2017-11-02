d4a6a4d1e1b90f2dc8d1c08fb812c0ff29ba01d2
Calvin Dominique Cox III <calvinc@g.clemson.edu>
2017-11-01 21:05:07 -0400

    Sucessful Dictionary Attempt
    Didn't realize I had a draft but I'm glad I didn't look at it. The problem was simple and it looked like I made it really complicated.

--- Introductory_Exercises/Dictionaries.py
+++ Introductory_Exercises/Dictionaries.py
@@ -1,33 +1,23 @@
-#Intro to Python Dictonary
+#Intro to Python Dictionary
+#Calvin D. Cox
+#11/01/2017
 
-#http://rosalind.info/problems/ini6/
 #Given: A string ss of length at most 10000 letters.
 #Return: The number of occurrences of each word in ss, where words are separated by spaces. Words are case-sensitive, and the lines in the output can be in any order.
-#****this solution did not work but based on hints and comparing the answer put into a dictionary this gives the right count. Something about the answer submission for this question is buggy. May try to submit later.
-import json
 
-#Reading one line from input file
-with open('rosalind_ini6.txt','r') as file:
-    cache = file.readline()
-    cache_break = cache.split(" ")
-    
-del cache_break[-1]
-cache_break.append('be')
-newdict = {}
+file = open('exampletext.txt')
+words = file.readline()
 
-for word in cache_break:
-    if word in newdict:
-        newdict[word] = newdict[word] + 1
+words = words.rstrip("\n")
+words = words.split(' ')
+worddict = {}
+
+
+for i in words:
+    if i in worddict:
+        worddict[i] += 1
     else:
-        newdict[word] = 1
-
-
-for x in newdict:
-    output = open('r_outputm.txt', 'a')
-    k = x + " " + str(newdict[x])
-    print(k.strip(), file=output)
-  
-
-print(k.strip(), file=output)
-
-   
+        worddict[i] = 1
+        
+for key, value in worddict.items():
+    print (str(key) + ' ' + str(value))
