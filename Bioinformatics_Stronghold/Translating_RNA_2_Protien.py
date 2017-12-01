1921a97f59f7ef8e4ee81ead6c529a3df9f4fb69
Calvin Dominique Cox III <calvinc@g.clemson.edu>
2017-11-30 21:36:42 -0500

    Create Translating_RNA_2_Protien.py
    drafting

---
+++ Bioinformatics_Stronghold/Translating_RNA_2_Protien.py
@@ -0,0 +1,14 @@
+#Translating RNA into Protein
+#Calvin D. Cox
+#11/30/2017
+
+import pandas as pd
+
+#"With" is apparently more pythonic than "open"
+#Opening file with RNA string
+with open("example.txt") as f:
+    rna = f.readline()
+    
+#Want to open file with rna table provided in the puzzle and then just get a loop to search a dictionary.
+rnatoamino_table = 
+    
