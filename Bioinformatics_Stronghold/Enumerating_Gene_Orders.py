e190ac106ce011c93423b2eb1ccf4838727faf7b
Calvin Dominique Cox III <calvinc@g.clemson.edu>
2017-12-10 14:48:26 -0500

    Update Enumerating_Gene_Orders.py
    Enumerating Gene  Orders

--- Bioinformatics_Stronghold/Enumerating_Gene_Orders.py
+++ Bioinformatics_Stronghold/Enumerating_Gene_Orders.py
@@ -3,6 +3,7 @@
 #12/10/17
 
 from itertools import permutations as perm
+import re
 
 #Given: A positive integer n≤7n≤7.
 #Return: The total number of permutations of length nn, followed by a list of all such permutations (in any order).
@@ -10,20 +11,29 @@ from itertools import permutations as perm
 #Open File with Number
 with open('exampletext.txt', 'r') as f:
     number = int(f.readline())
- 
+
+#Creating a populated list for the permutations program
 numlist = []
 i = 1
 while i <= number:
     numlist.append(i)
     i += 1
-    
+
+#Creating permutation File, working with num_perm later in the printing process didn't work so I immediately put it into a workable list
+perm_store = []
 num_perm = perm(numlist, number)
-length_perm = len(list(num_perm))
-
-
-print(len(list(num_perm)))
-
 for i in list(num_perm):
-    print(i)
+    perm_store.append(i)
+
+
+length_perm = len(perm_store)
+
+#Plugging results into file, while I was testing other numbers I noticed the permutations can get quite large so I opted to write things into a file.
+with open('resulttext.txt', 'w') as rt:
+    rt.write(str(length_perm)+'\n')
+    for j in perm_store:
+        rt.write(re.sub("[^0-9 ]", "", str(j)) + '\n') #Strips everything but spaces and numbers
+
+
     
     
