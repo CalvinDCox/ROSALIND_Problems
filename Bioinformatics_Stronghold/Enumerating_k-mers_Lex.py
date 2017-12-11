6af7766f3eb4a2a71bfed5fab1217aad8dbf9102
Calvin Dominique Cox III <calvinc@g.clemson.edu>
2017-12-10 20:59:05 -0500

    Update Enumerating_k-mers_Lex.py
    switched perm for products.

--- Bioinformatics_Stronghold/Enumerating_k-mers_Lex.py
+++ Bioinformatics_Stronghold/Enumerating_k-mers_Lex.py
@@ -5,15 +5,18 @@
 #Given: A collection of at most 10 symbols defining an ordered alphabet, and a positive integer nn (n≤10n≤10).
 #Return: All strings of length nn that can be formed from the alphabet, ordered lexicographically (use the standard order of symbols in the English alphabet).
 
-from itertools import permutations as perm
+from itertools import product as prod
 import re
 
 #Loading in the Sequence Files
 with open('exampletext.txt', 'r') as et:
     data_set = et.readlines()
 
+#Store permutation in an acceptable format to be sorted
 perm_store = []
-num_perm = perm(re.sub("[^A-Z]", "", data_set[0]), int(data_set[1]))
+num_perm = prod(re.sub("[^A-Z]", "", data_set[0]), repeat=int(data_set[1]))
 for i in list(num_perm):
     perm_store.append(re.sub("[^A-Z]", "", str(i)))
-
+    
+#Sort permutation list
+sorted(perm_store)
