3d05eea4fd5c9a23b3b80eeb3ca998ce05460772
Calvin Dominique Cox III <calvinc@g.clemson.edu>
2018-01-18 21:00:57 -0500

    Final
    Cleaned up newlines in strings, working now

--- Bioinformatics_Stronghold/Calculating_Protein_Mass.py
+++ Bioinformatics_Stronghold/Calculating_Protein_Mass.py
@@ -4,21 +4,25 @@
 
 #Given: A protein string PP of length at most 1000 aa.
 #Return: The total weight of PP. Consult the monoisotopic mass table.
+
 import re
 
+#Load Files
 with open('exampletext.txt', 'r') as aa:
     P = aa.readline()
     
 with open('Mono_pro.txt', 'r') as tf:
     pro_table = tf.readlines()
-    
-mass = 0    
 
-#pull info from table and get the mass from that row.
+#Create a dictionary from the table, I cheated a bit and loaded the table from the site into a notepad file. 
+aminos = {}    
+for n in pro_table:
+    aminos[n[0]] = float(re.sub("[^0123456789\.]", "", n.rstrip())) #much cleaner way to do this?
+
+#Calculating mass
+mass = 0    
+P = P.rstrip()
 for a in P:
-    for amino in pro_table:
-        if a in amino:
-            mass += float(re.sub("[^0123456789\.]", "", amino)) 
-        
-            
-            
+    mass += aminos[a]
+ 
+print(str(round(mass, 3)))
